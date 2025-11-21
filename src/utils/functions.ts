facbc22dc835c9cf9e66937d70d79fa2a7e8be79
Yurii Ryzhkin <dfm4ik@Yuriis-MacBook-Pro-2.local>
2025-11-21 21:28:53 +0100

    changed worklow with deliting old containers

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -21,7 +21,7 @@ export type Replacment = {
 
 export function formatTextForProperMarkdown(text: string): string {
 
-	const change_elements: Array<Replacment> = [{ str1: "_", str2: "__" }];
+	const change_elements: Array<Replacment> = [{ str1: "_", str2: "__" }, { str1: ".", str2: "\\." }, { str1: "=", str2: "\\=" }];
 	for (let i: number = 0; i < change_elements.length; i++) {
 		text = text.replaceAll(
 			change_elements[i]?.str1 as string,
